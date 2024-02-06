ddd0d6803fb542257ea05e2c10eccb9112b96751
gillgate <gillgate@yandex.ru>
2024-02-06 17:36:08 +0300

    Update manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import './App.css';
-import { TonConnectButton, useTonAddress } from '@tonconnect/ui-react';
-import { useMainContract } from './hooks/useMainContract';
-import { useTonConnect } from './hooks/useTonConnect';
-import { fromNano } from 'ton-core';
-import WebApp from '@twa-dev/sdk';
+import "./App.css";
+import { TonConnectButton, useTonAddress } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
+import { fromNano } from "ton-core";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
-  const { 
-    contract_address, 
-    contract_balance, 
-    counter_value, 
+  const {
+    contract_address,
+    contract_balance,
+    counter_value,
     owner_address,
-    sendIncrement, 
+    sendIncrement,
     sendDeposit,
-    sendWithdrawalRequest
+    sendWithdrawalRequest,
   } = useMainContract();
 
   const { connected } = useTonConnect();
@@ -26,53 +26,82 @@ function App() {
     WebApp.showAlert("Hey there!");
   };
 
+  WebApp.expand();
+
+  console.log(WebApp);
+
+  const root = document.querySelector(":root");
+  root.style.setProperty("--color-scheme", WebApp.colorScheme);
+
   return (
     <div className="App">
       <div className="App__connect">
         <TonConnectButton />
       </div>
       <div>
+        {WebApp.platform !== "unknown" && (
+          <div className="Card">
+            <div>{WebApp.platform}</div>
+            <button
+              onClick={() => showAlert()}
+              type="button"
+              className="App__send"
+            >
+              Show alert
+            </button>
+          </div>
+        )}
         <div className="Card">
-          <div>{WebApp.platform}</div>
-          <button onClick={() => showAlert()} type="button" className='App__send'>
-            Show alert
-          </button>
-        </div>
-        <div className='Card'>
           <b>Our contract Address</b>
-          <div className='Hint'>{contract_address}</div>
+          <div className="Hint">{contract_address}</div>
 
-          {contract_balance && <>
-            <b>Our contract Balance</b>
-            <div className='Hint'>{fromNano(contract_balance)}</div>
-          </>}
+          {contract_balance && (
+            <>
+              <b>Our contract Balance</b>
+              <div className="Hint">{fromNano(contract_balance)}</div>
+            </>
+          )}
         </div>
 
-        <div className='Card'>
-            <b>Counter Value</b>
-            <div>{counter_value ?? "Loading..."}</div>
+        <div className="Card">
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
         </div>
 
-        {connected && (<>
+        {connected && (
+          <>
             <div>
-                <button onClick={() => sendIncrement()} type="button" className='App__send'>
-                    Send increment
-                </button>
+              <button
+                onClick={() => sendIncrement()}
+                type="button"
+                className="App__send"
+              >
+                Send increment
+              </button>
 
-                <button onClick={() => sendDeposit()} type="button" className='App__send'>
-                    Send 1 TON
-                </button>
+              <button
+                onClick={() => sendDeposit()}
+                type="button"
+                className="App__send"
+              >
+                Send 1 TON
+              </button>
             </div>
-            
+
             {isOwner && (
-                <button onClick={() => sendWithdrawalRequest()} type="button" className='App__withdraw'>
-                    Widthdraw 0.6 TON
-                </button>
+              <button
+                onClick={() => sendWithdrawalRequest()}
+                type="button"
+                className="App__withdraw"
+              >
+                Widthdraw 0.6 TON
+              </button>
             )}
-        </>)}
+          </>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = "https://raw.githubusercontent.com/markokhman/func-course-chapter-5-code/master/public/manifest.json";
+const manifestUrl = "https://gillgate.github.io/first-dapp/manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
