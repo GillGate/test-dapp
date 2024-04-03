6e88490d161f2c4e66972f52cceabf2ce78df38e
gillgate <gillgate@yandex.ru>
2024-04-03 18:56:44 +0300

    Build fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "DAPP",
+  base: '/first-dapp/',
   server: {
     open: true
   },
